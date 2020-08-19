c8d899f3e2c2e04a3b17d05e3dc3714aedbff427
Sho Ando <spdtas0223@gmail.com>
2020-08-19 15:37:28 +0900

    完了

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,24 @@
 class OrdersController < ApplicationController
+  def index
+    @orders = Order.all
+  end
+
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to root_path
+    else
+      render action: :new
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:title, :content, :last_day, :deadline, :award).merge(company_id: current_company.id)
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,10 @@
 class Order < ApplicationRecord
   belongs_to :company
-  with_options presense: true do
+  with_options presence: true do
     validates :title, length: { maximum: 40 }
     validates :content, length: { maximum: 1000 }
     validates :last_day, :deadline, :company_id
+    validates :award, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
+                                      message: 'が可能な範囲を超えています' }
   end
 end
