b592f25f4c3fcaa1628cf5d9b3595cf8de04a08c
Sho Ando <spdtas0223@gmail.com>
2020-08-28 14:12:54 +0900

    cardモデルの単体テスト

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,6 @@
 class Card < ApplicationRecord
   belongs_to :company
+  with_options presence: true do
+    validates :card_token, :customer_token
+  end
 end
--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :card do
-    
+    card_token { 'tok_0268129036189230741361093713198365'}
+    customer_token { 'cus_dsjfbvlairuailrbavldjvabkfsvair' }
+    association :company
   end
 end
