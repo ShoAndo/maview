e43f3430d90a7bfe93749048c5662aa98340e929
Sho Ando <spdtas0223@gmail.com>
2020-09-01 13:17:40 +0900

    修正

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -4,21 +4,16 @@ class CompaniesController < ApplicationController
     @company = Company.find(params[:id])
     if Outline.find_by(company_id: @company.id)
       @outline = Outline.find_by(company_id: @company.id)
-    else
-      return
     end
 
     if Card.find_by(company_id: @company.id)
-      @card = Card.find_by(company_id: @company.id)
-    else
-      return
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      card = Card.find_by(company_id: @company.id)
+
+      customer = Payjp::Customer.retrieve(card.customer_token)
+      @card = customer.cards.first
     end
 
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-    card = Card.find_by(company_id: @company.id)
-
-    customer = Payjp::Customer.retrieve(card.customer_token)
-    @card = customer.cards.first
   end
 
   def edit
